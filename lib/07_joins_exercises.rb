ee11d0c70a5fe63c6e020d3938f77afb9ce7ab8e
Jason Reeder <info@jasonreeder.com>
2017-02-06 15:42:22 -0800

    Add

--- lib/07_joins_exercises.rb
+++ lib/07_joins_exercises.rb
@@ -167,6 +167,20 @@ def films_by_cast_size
   # List the films released in the year 1978 ordered by the number of actors
   # in the cast (descending), then by title (ascending).
   execute(<<-SQL)
+    SELECT
+      title,
+      COUNT(*) AS "Cast size"
+    FROM
+      movies
+    JOIN castings
+      ON movies.id = castings.movie_id
+    WHERE
+      yr = 1978
+    GROUP BY
+      title
+    ORDER BY
+      COUNT(*) DESC,
+      title
   SQL
 end
 
